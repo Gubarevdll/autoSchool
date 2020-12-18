bf19304908307576ac9d9fb7fdfe277213f815f3
Gubarevdll <Danylo.Hubariev@nixsolution.com>
2020-12-18 23:23:23 +0200

    Module 2 task 4, subtask1 is added

--- src/main/java/module2/task4/Taks1.java
+++ src/main/java/module2/task4/Taks1.java
@@ -1,3 +1,8 @@
+/*Дана строка. Вывести первый, последний и средний (если он есть) символы.
+Напишите программу, которая выводит часть строки до первой встреченной точки, включая точку.
+Также предусмотрите вывод количества пробелов.
+ */
+
 package module2.task4;public class Taks1 {
 
     public static void main(String[] args) {
@@ -9,7 +14,15 @@ package module2.task4;public class Taks1 {
 
         System.out.println("First letter - " + first_char +" last letter - " + last_char);
 
+        int value = x.indexOf('.');
+        System.out.println(x.substring(0, value+1));
 
+        int spaceCount = 0;
+        for (char c : x.toCharArray()) {
+            if (c == ' ') {
+                spaceCount++;
+            }
+        }
+        System.out.println(spaceCount);
     }
-
 }
