2285c380ae60055750c38a0ad954d8a3b987fe87
Danil_Gubarev <danylo.hubariev@nixsolution.com>
2020-12-18 12:42:57 +0200

    Module2, task3, subtask4 added

---
+++ src/main/java/module2/task3/Subtask4.java
@@ -0,0 +1,23 @@
+/*4 Даны три переменные a, b и c.
+        Изменить значения этих переменных так, ч
+        тобы в a хранилось значение a+b, в b хранилась разность
+        старых значений c−a, а в c хранилось сумма старых значений a+b+c.
+        Например, a=0, b=2, c=5, тогда новые значения a=2, b=5 и c=7.*/
+
+package module2.task3;
+
+public class Subtask4 {
+    public static void main(String[] args) {
+
+
+        int a=Subtask4.a, b=Subtask4.c-Subtask4.a, c=Subtask4.a+Subtask4.b+Subtask4.c;
+
+
+        System.out.println(a + " " + b + " " + c);
+
+        }
+
+        static int a = 0, b = 2, c = 5;
+
+    }
+
