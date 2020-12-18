6a43a933d9d49732fbd759699dc0a38d255ff5ca
Gubarevdll <Danylo.Hubariev@nixsolution.com>
2020-12-18 21:03:09 +0200

    Module 2 task 4, subtask1 (partially) is added

---
+++ src/main/java/module2/task4/Taks1.java
@@ -0,0 +1,15 @@
+package module2.task4;public class Taks1 {
+
+    public static void main(String[] args) {
+
+        String x = "random.w o r d";
+
+        String first_char = x.substring(0,1);
+        String last_char = x.substring(x.length()-1);
+
+        System.out.println("First letter - " + first_char +" last letter - " + last_char);
+
+
+    }
+
+}
