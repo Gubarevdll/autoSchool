c4af945504d4004800f54c57157ee869e858a28b
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 17:47:03 +0200

    Module 2, task6, Subtask14  added

---
+++ src/main/java/module2/task6/Subtask14.java
@@ -0,0 +1,21 @@
+package module2.task6;
+
+public class Subtask14 {
+    public static void main(String[] args) {
+        int m = 2;
+        int n = 6;
+
+
+        for (int a = 0; a < m; a++) {
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.print(8);
+            }
+        }
+    }
+    }
+
+    /* Много раз слышал, что нельзя использовать цикл внутри цикла, по пока неизвестным мне причинам,
+    но тут пришлось использовать
+    Все же хотелось бы узнать почему так нельзя:) */
+
