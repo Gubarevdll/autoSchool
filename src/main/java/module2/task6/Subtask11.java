0e21e11876ec5d85caaec80e4325ea3714127d1d
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 17:00:48 +0200

    Module 2, task6, Subtask11  added

---
+++ src/main/java/module2/task6/Subtask11.java
@@ -0,0 +1,12 @@
+package module2.task6;
+
+public class Subtask11 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i<10){
+            System.out.println("SSSSSSSSSS");
+            i++;
+        }
+    }
+}
