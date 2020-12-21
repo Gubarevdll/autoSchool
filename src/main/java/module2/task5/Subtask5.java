859b2c69a42d2b5a3ff71986b8fbb8f805bf486e
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 13:32:58 +0200

    Module 2, task5, subtask5  added

---
+++ src/main/java/module2/task5/Subtask5.java
@@ -0,0 +1,46 @@
+package module2.task5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Subtask5 {
+    public static void main(String[] args) throws Exception {
+        int[] big_arr = initializeArray(20);
+        int[] first_arr = new int[10];
+        int[] second_arr = new int[10];
+
+        for (int i=0; i<first_arr.length; i++){
+            first_arr[i]=big_arr[i];
+        }
+
+        for (int i=0; i<second_arr.length; i++){
+            second_arr[i]=big_arr[i+first_arr.length];
+        }
+        System.out.println("First array:");
+        for (int i=0; i<first_arr.length; i++){
+            System.out.println(first_arr[i]);
+        }
+        System.out.println("Second array:");
+        for (int i=0; i<second_arr.length; i++){
+            System.out.println(second_arr[i]);
+        }
+    }
+
+    static int[] initializeArray(int size) throws Exception {
+
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String a = reader.readLine();
+            try {
+                int x = Integer.parseInt(a.trim());
+                array[i] = x;
+
+            } catch (Exception e) {
+                i++;
+            }
+        }
+        return array;
+    }
+}
