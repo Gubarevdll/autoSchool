2f17edbfb1809df3ddacc419d8615797f8e60dfe
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 13:52:54 +0200

    Module 2, task6, subtask1  added

---
+++ src/main/java/module2/task6/Subtask1.java
@@ -0,0 +1,26 @@
+package module2.task6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Subtask1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = reader.readLine();
+        String b = reader.readLine();
+
+        int x = Integer.parseInt(a.trim());
+        int z = Integer.parseInt(b.trim());
+
+        int result = 0;
+
+        if (x>z){
+            result=x;
+        }
+        else {
+            result=z;
+        }
+
+        System.out.println(result);
+}}
