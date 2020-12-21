1768cce31d68b17ba4e367bb3fdc8f10942bec53
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 15:56:33 +0200

    Module 2, task6, subtask3  added

---
+++ src/main/java/module2/task6/Subtask3.java
@@ -0,0 +1,26 @@
+package module2.task6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Subtask3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+        int[] arr = new int[3];
+
+        arr = new int[]{a, b, c};
+
+        for (int i=arr.length-1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
