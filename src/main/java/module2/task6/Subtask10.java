04b19d3c30f74d259c285bc79ca5ab2188a77c15
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 16:58:18 +0200

    Module 2, task6, Subtask10  added

---
+++ src/main/java/module2/task6/Subtask10.java
@@ -0,0 +1,25 @@
+package module2.task6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Subtask10 {
+    public static void main(String[] args)throws Exception{
+        System.out.println("Введите строку:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = reader.readLine();
+
+        System.out.println("Введите число повторений");
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(System.in));
+
+        String b = reader2.readLine();
+        int N = Integer.parseInt(b.trim());
+
+        while (N>0){
+            System.out.println(a);
+            N--;
+        }
+
+    }
+}
