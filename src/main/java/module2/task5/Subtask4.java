4f65abfd740e879ab711ff8c44fd621151dd13dc
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 12:53:54 +0200

    Module 2, task5, subtask4  added

---
+++ src/main/java/module2/task5/Subtask4.java
@@ -0,0 +1,36 @@
+package module2.task5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Subtask4 {
+    public static void main(String[] args) throws Exception {
+        int[] arr = initializeArray(10);
+
+        for (int i=arr.length-1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+
+
+    static int[] initializeArray(int size) throws Exception{
+
+        int[] array = new int[size];
+        for (int i=0; i<array.length; i++){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String a = reader.readLine();
+            try {
+                int x = Integer.parseInt(a.trim());
+                array[i] = x;
+
+            }
+            catch (Exception e){
+                i++;
+            }
+        }
+        return array;
+    }
+}
