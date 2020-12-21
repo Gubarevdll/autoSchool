09ea61a7b4cac2c4af35b42a3bfbbe14c543f1ba
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 16:08:45 +0200

    Module 2, task6, subtask5  added

---
+++ src/main/java/module2/task6/Subtask5.java
@@ -0,0 +1,26 @@
+package module2.task6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Subtask5 {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите имя:");
+        String name = reader.readLine();
+        System.out.println("Введите возраст");
+        String age_s = reader.readLine();
+        int age = Integer.parseInt(age_s.trim());
+
+
+        if (age<=18){
+            System.out.println("Подрасти еще");
+        }
+        else if (age>=20){
+            System.out.println("И 18-ти достаточно");
+        }
+
+    }
+}
