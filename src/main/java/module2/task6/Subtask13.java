943dddd489daabbc10406e5f387cfb9da5d11cab
hubariev <danylo.hubariev@nixsolutions.com>
2020-12-21 17:33:30 +0200

    Module 2, task6, Subtask13  added

---
+++ src/main/java/module2/task6/Subtask13.java
@@ -0,0 +1,12 @@
+package module2.task6;
+
+public class Subtask13 {
+    public static void main(String[] args) {
+
+        for (int i=0; i<=100; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
