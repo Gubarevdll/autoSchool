13eec036661702021ae5ae477e285861c37c3f10
hubariev <danylo.hubariev@nixsolutions.com>
2021-01-04 18:46:26 +0200

    Module 5, task2, subtask2(partially) added

---
+++ src/main/java/module5/task2/Subtask2.java
@@ -0,0 +1,21 @@
+package module5.task2;
+
+import java.util.HashMap;
+
+public class Subtask2 {
+    public static void main(String[] args) {
+
+        HashMap<String, String> map = new HashMap();
+        map.put("арбуз", "ягода");
+        map.put("банан", "трава");
+        map.put("вишня", "ягода");
+        map.put("груша", "фрукт");
+        map.put("дыня", "овощ");
+        map.put("ежевика", "куст");
+        map.put("жень-шень", "корень");
+        map.put("земляника", "ягода");
+        map.put("ирис", "цветок");
+        map.put("картофель", "клубень");
+
+    }
+}
