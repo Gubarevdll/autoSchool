3c04e728c361a5611b753ff213a0e9f2325b0daa
hubariev <danylo.hubariev@nixsolutions.com>
2021-01-04 16:10:43 +0200

    logs config added

--- src/main/java/module4/task3/Logic.java
+++ src/main/java/module4/task3/Logic.java
@@ -3,24 +3,20 @@ package module4.task3;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Subtask2 {
+public class Logic {
     static String line;
-    final static Logger LOGGER = LogManager.getLogger(Subtask2.class.getName());
-
-    public static void main(String[] args) {
-        publicMethod();
-    }
+    final static Logger LOGGER = LogManager.getLogger(Logic.class.getName());
 
 
         static private void privateMethod(){
             line = "original line";
-            LOGGER.info("Private method has been called");
+            LOGGER.warn("Private method has been called");
         }
 
         static public void publicMethod(){
             privateMethod();
             line = line + " with changes";
-            LOGGER.info("Public method has been called");
+            LOGGER.warn("Public method has been called");
         }
 
 }
---
+++ src/main/java/module4/task3/Main.java
@@ -0,0 +1,8 @@
+package module4.task3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logic.publicMethod();
+    }
+}
