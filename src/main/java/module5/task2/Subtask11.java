6cedeb9e0a713fca54351c9633b086bdc89abbb6
hubariev <danylo.hubariev@nixsolutions.com>
2021-01-05 12:11:03 +0200

    Module 5, task2, subtask 11 added

---
+++ src/main/java/module5/task2/Subtask11.java
@@ -0,0 +1,26 @@
+package module5.task2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Subtask11 {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            String a = reader.readLine();
+            list.add(a);
+        }
+
+        for (int i = 0; i < 13; i++){
+            String buff = list.get(4);
+            list.remove(4);
+            list.add(0, buff);
+        }
+
+        System.out.println(list);
+    }
+}
--- src/main/java/module5/task2/Subtask9.java
+++ src/main/java/module5/task2/Subtask9.java
@@ -1,4 +1,26 @@
 package module5.task2;
 
+import java.util.ArrayList;
+
 public class Subtask9 {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+
+        int shortest = list.get(0).length();
+        String result = list.get(0);
+        for (int i = 0; i < list.size(); i++){
+            if (shortest > list.get(i).length()){
+                shortest = list.get(i).length();
+                result = list.get(i);
+            }
+        }
+        System.out.println(result);
+    }
 }
+
