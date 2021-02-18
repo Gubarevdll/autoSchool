7d25ed5a3d3cdcc482ba5f36e74df277865964c9
hubariev <danylo.hubariev@nixsolutions.com>
2021-02-18 13:46:31 +0200

    Moodule 11, Webdriver partially

--- src/test/java/Module11_Selenium/Webdriver.java
+++ src/test/java/Module11_Selenium/Webdriver.java
@@ -1,10 +1,16 @@
 package Module11_Selenium;
 
 import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Webdriver {
 
@@ -14,6 +20,7 @@ public class Webdriver {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://automationpractice.com/");
+        SoftAssert softAssert = new SoftAssert();
 
         WebElement searchField = driver.findElement(By.xpath("//*[@id=\"search_query_top\"]"));
         searchField.sendKeys("Summer");
@@ -30,5 +37,22 @@ public class Webdriver {
         WebElement highestFirst = driver.findElement(By.xpath("//*[@id=\"selectProductSort\"]/option[3]"));
         highestFirst.click();
 
+        ArrayList<String> pricesFromBoard = new ArrayList<String>();
+        pricesFromBoard.add(driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[1]/span[2]")).getText());
+        pricesFromBoard.add(driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[2]/div[1]/span")).getText());
+        pricesFromBoard.add(driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[3]/div/div[2]/div[1]/span[2]")).getText());
+        pricesFromBoard.add(driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[4]/div/div[2]/div[1]/span")).getText());
+        ArrayList<String> sorted = new ArrayList(pricesFromBoard);
+        sorted.sort(Comparator.reverseOrder());
+
+        softAssert.assertTrue(sorted==pricesFromBoard);
+
+        String firstElementName = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a")).getText();
+        String firstElementPrice = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[1]/span[2]")).getText();
+
+        WebElement addToCart = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span"));
+
+        softAssert.assertAll();
     }
+
 }
