d50affa2ad71e420e43ff7fb2338e781db646fcd
hubariev <danylo.hubariev@nixsolutions.com>
2021-02-18 18:22:45 +0200

    Moodule 11, Webdriver partially

--- src/test/java/Module11_Selenium/Webdriver.java
+++ src/test/java/Module11_Selenium/Webdriver.java
@@ -1,12 +1,15 @@
 package Module11_Selenium;
 
 import org.junit.Test;
-import org.junit.Assert;
-import static org.junit.Assert.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import java.util.ArrayList;
@@ -14,16 +17,18 @@ import java.util.Comparator;
 
 public class Webdriver {
 
+
+    @Parameters ({"Summer"})
+
     @Test
-    public void testLocator() {
+    public void testLocator(String Summer) {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://automationpractice.com/");
-        SoftAssert softAssert = new SoftAssert();
 
         WebElement searchField = driver.findElement(By.xpath("//*[@id=\"search_query_top\"]"));
-        searchField.sendKeys("Summer");
+        searchField.sendKeys(Summer);
 
         WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"searchbox\"]/button"));
         searchButton.click();
@@ -45,14 +50,40 @@ public class Webdriver {
         ArrayList<String> sorted = new ArrayList(pricesFromBoard);
         sorted.sort(Comparator.reverseOrder());
 
-        softAssert.assertTrue(sorted==pricesFromBoard);
+        SoftAssert assertion = new SoftAssert();
+        assertion.assertTrue(sorted==pricesFromBoard);
 
         String firstElementName = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a")).getText();
+        System.out.println(firstElementName);
         String firstElementPrice = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[1]/span[2]")).getText();
 
-        WebElement addToCart = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span"));
+        WebElement ele = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]"));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", ele);
 
-        softAssert.assertAll();
+        WebDriverWait wait10 = new WebDriverWait(driver, 10);
+        WebElement closeWindowButton = wait10.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[1]/span")));
+        JavascriptExecutor executor2 = (JavascriptExecutor)driver;
+        executor2.executeScript("arguments[0].click();", closeWindowButton);
+
+        WebElement openCart = wait10.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a")));
+        openCart.click();
+
+        wait10.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id=\"total_price\"]"))));
+        WebElement tatalElement = driver.findElement(By.xpath("//*[@id=\"total_price\"]"));
+        String totalString = tatalElement.getText();
+        System.out.println("Total sum = "+ totalString);
+
+        wait10.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id=\"product_5_19_0_0\"]/td[2]/p/a"))));
+        WebElement inCartName = driver.findElement(By.xpath("//*[@id=\"product_5_19_0_0\"]/td[2]/p/a"));
+        String inCartNameString = inCartName.getText();
+        System.out.println("Name = "+ inCartNameString);
+
+        assertion.assertTrue(inCartNameString.equals(firstElementName));
+        assertion.assertTrue(totalString==firstElementPrice);
+
+        assertion.assertAll();
     }
+
 
 }
